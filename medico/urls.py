1b06d9e40d1b9deff9a129adc8dc9159d4b275cc
qwerty11211 <123802227+qwerty11211@users.noreply.github.com>
2023-02-13 04:49:19 +0530

    Add files via upload

---
+++ medico/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('user.urls')),
+    path('doctor/', include('doctor.urls')),
+
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
